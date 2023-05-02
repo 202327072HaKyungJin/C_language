978dbb357398eccd5fa2268add70a510c03e4b91
202327072HaKyungJin/c <bluetiger2004@naver.com>
2023-05-02 15:42:22 +0900

    3월 28일

---
+++ 0328.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num:
+        num=18;
+    printf("주인공의 공격력을 입력하세요: ");
+    scanf("%d" , &num);
+
+     printf("입력한 정수는 %d입니다.", num);
+
+     if(hp>18) {
+               printf("주인공이 적을 공격하여 &d의 데미지를 입혔습니다.",num):
+}
+  else{ 
+        printf("hp가 %d의 데미지보다 큽니다. \n적이 주인공을 공격하여 주인공이 뒤졌습니다.",num);
+    }
+
+ return 0;
+
+}   
