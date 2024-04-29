237baa85c4a30a6ca791c47e1aeed8d312f4c1ee
202327072HaKyungJin <132327757+202327072HaKyungJin@users.noreply.github.com>
2024-04-29 15:31:04 +0900

    Add files via upload

---
+++ Lecture08.js
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.9.34723.18
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "Lecture08", "Lecture08\Lecture08.vcxproj", "{5AA906BE-3D38-4652-9C4E-B7911B37A688}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{5AA906BE-3D38-4652-9C4E-B7911B37A688}.Debug|x64.ActiveCfg = Debug|x64
+		{5AA906BE-3D38-4652-9C4E-B7911B37A688}.Debug|x64.Build.0 = Debug|x64
+		{5AA906BE-3D38-4652-9C4E-B7911B37A688}.Debug|x86.ActiveCfg = Debug|Win32
+		{5AA906BE-3D38-4652-9C4E-B7911B37A688}.Debug|x86.Build.0 = Debug|Win32
+		{5AA906BE-3D38-4652-9C4E-B7911B37A688}.Release|x64.ActiveCfg = Release|x64
+		{5AA906BE-3D38-4652-9C4E-B7911B37A688}.Release|x64.Build.0 = Release|x64
+		{5AA906BE-3D38-4652-9C4E-B7911B37A688}.Release|x86.ActiveCfg = Release|Win32
+		{5AA906BE-3D38-4652-9C4E-B7911B37A688}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {A8F75EE0-E4B8-4DCD-83D1-0CE32663DC80}
+	EndGlobalSection
+EndGlobal
