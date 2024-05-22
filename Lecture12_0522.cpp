25565fe8629aa80ed78186c88958e4e6627f0d21
202327072HaKyungJin <132327757+202327072HaKyungJin@users.noreply.github.com>
2024-05-22 15:48:24 +0900

    Add files via upload

---
+++ Lecture12_0522.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<chrono>
+#include<thread>
+
+using namespace std;
+
+int main()
+{
+	int count = 0;
+
+	chrono::steady_clock::time_point start = chrono::steady_clock::now();
+	chrono::steady_clock::time_point end = chrono::steady_clock:now();
+
+	while (count < 100)
+	{
+		count++;
+		cout << count << endl;
+
+
+		chrono::steady_clock::duration dur = end - start;
+		int duration_ms = chrono::duration_cast<chrono::milliseconds>(dur).count();
+
+
+		if (duration_ms > 1000)
+		end = chrono::steady_clock::now();
+	
+	}
+
+
+}
